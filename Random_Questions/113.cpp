0ca6d7dee094f186a5afd0aabbbec9e47f81f454
Ravi Saini <rs8228495@gmail.com>
2026-04-03 21:01:53 +0530

    DSA

---
+++ Random_Questions/113.cpp
@@ -0,0 +1,77 @@
+// 113. Path Sum II
+
+// Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where the sum of the node values in the path equals targetSum. Each path should be returned as a list of the node values, not node references.
+
+// A root-to-leaf path is a path starting from the root and ending at any leaf node. A leaf is a node with no children.
+
+// Example 1:
+
+// Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+// Output: [[5,4,11,2],[5,8,4,5]]
+// Explanation: There are two paths whose sum equals targetSum:
+// 5 + 4 + 11 + 2 = 22
+// 5 + 8 + 4 + 5 = 22
+// Example 2:
+
+// Input: root = [1,2,3], targetSum = 5
+// Output: []
+// Example 3:
+
+// Input: root = [1,2], targetSum = 0
+// Output: []
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 5000].
+// -1000 <= Node.val <= 1000
+// -1000 <= targetSum <= 1000
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    void solve(TreeNode *root, int targetSum, vector<vector<int>> &result,
+               vector<int> &path)
+    {
+        if (!root)
+            return;
+
+        path.push_back(root->val);
+        if (!root->left && !root->right && targetSum == root->val)
+        {
+
+            result.push_back(path);
+            path.pop_back();
+            return;
+        }
+
+        solve(root->left, targetSum - root->val, result, path);
+        solve(root->right, targetSum - root->val, result, path);
+
+        path.pop_back();
+    }
+
+    vector<vector<int>> pathSum(TreeNode *root, int targetSum)
+    {
+
+        vector<vector<int>> result;
+        vector<int> path;
+
+        solve(root, targetSum, result, path);
+
+        return result;
+    }
+};
+
+// TC-->O(N^2)
