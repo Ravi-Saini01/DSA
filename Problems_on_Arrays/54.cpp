c2cdd99459e2e42ad1dd50093c994bbf17b74fcd
Ravi Saini <rs8228495@gmail.com>
2026-04-21 20:28:35 +0530

    DSA

--- Problems_on_Arrays/54.cpp
+++ Problems_on_Arrays/54.cpp
@@ -2,19 +2,14 @@
 
 // Given an m x n matrix, return all elements of the matrix in spiral order.
 
- 
-
 // Example 1:
-
 
 // Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 // Output: [1,2,3,6,9,8,7,4,5]
 // Example 2:
 
-
 // Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
 // Output: [1,2,3,4,8,12,11,10,9,5,6,7]
- 
 
 // Constraints:
 
@@ -23,3 +18,54 @@
 // 1 <= m, n <= 10
 // -100 <= matrix[i][j] <= 100
 
+class Solution
+{
+public:
+    vector<int> spirallyTraverse(vector<vector<int>> &mat)
+    {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+
+        int left = 0, right = m - 1;
+        int top = 0, bottom = n - 1;
+
+        vector<int> ans;
+
+        while (left <= right && top <= bottom)
+        {
+            // left---right
+            for (int i = left; i <= right; i++)
+            {
+                ans.push_back(mat[top][i]);
+            }
+            top++;
+            // top--bottom
+            for (int i = top; i <= bottom; i++)
+            {
+                ans.push_back(mat[i][right]);
+            }
+            right--;
+            // right--left
+            if (top <= bottom)
+            {
+                for (int i = right; i >= left; i--)
+                {
+                    ans.push_back(mat[bottom][i]);
+                }
+                bottom--;
+            }
+            // bottom--top
+            if (left <= right)
+            {
+                for (int i = bottom; i >= top; i--)
+                {
+                    ans.push_back(mat[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+// TC-->O(N)
--- Problems_on_Arrays/560.cpp
+++ Problems_on_Arrays/560.cpp
@@ -1,11 +1,8 @@
 // 560. Subarray Sum Equals K
 
-
 // Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
 
 // A subarray is a contiguous non-empty sequence of elements within an array.
-
- 
 
 // Example 1:
 
@@ -15,7 +12,6 @@
 
 // Input: nums = [1,2,3], k = 3
 // Output: 2
- 
 
 // Constraints:
 
@@ -23,3 +19,26 @@
 // -1000 <= nums[i] <= 1000
 // -107 <= k <= 107
 
+class Solution
+{
+public:
+    int cntSubarrays(vector<int> &arr, int k)
+    {
+        // code here
+        int n = arr.size();
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+        int prefixsum = 0, cnt = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            prefixsum += arr[i];
+            int removel = prefixsum - k;
+            cnt += mpp[removel];
+            mpp[prefixsum] += 1;
+        }
+        return cnt;
+    }
+};
+// TC-->O(N)
+// SC-->O(N)
