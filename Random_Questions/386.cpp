31ffe07fd37d2a3d3539901072197599d916e2aa
Ravi Saini <rs8228495@gmail.com>
2026-07-07 21:11:46 +0530

    DSA

---
+++ Random_Questions/386.cpp
@@ -0,0 +1,52 @@
+// 386. Lexicographical Numbers
+
+// Given an integer n, return all the numbers in the range [1, n] sorted in lexicographical order.
+
+// You must write an algorithm that runs in O(n) time and uses O(1) extra space.
+
+// Example 1:
+
+// Input: n = 13
+// Output: [1,10,11,12,13,2,3,4,5,6,7,8,9]
+// Example 2:
+
+// Input: n = 2
+// Output: [1,2]
+
+// Constraints:
+
+// 1 <= n <= 5 * 104
+
+class Solution
+{
+public:
+    void solve(int curr, int n, vector<int> &res)
+    {
+        if (curr > n)
+            return;
+
+        res.push_back(curr);
+
+        for (int nextdig = 0; nextdig <= 9; nextdig++)
+        {
+            int nextnum = curr * 10 + nextdig;
+
+            if (nextnum > n)
+                return;
+
+            solve(nextnum, n, res);
+        }
+    }
+    vector<int> lexicalOrder(int n)
+    {
+        vector<int> res;
+
+        for (int num = 1; num <= 9; num++)
+        {
+            solve(num, n, res);
+        }
+
+        return res;
+    }
+};
+// TC-->O(N)
