a707842ddb635c3f7d2985e2b0f94f3654e9c1d1
Ravi Saini <rs8228495@gmail.com>
2026-08-21 17:23:33 +0530

    DSA

---
+++ Random_Questions/448.cpp
@@ -0,0 +1,51 @@
+// 448. Find All Numbers Disappeared in an Array
+
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+// Example 1:
+
+// Input: nums = [4,3,2,7,8,2,3,1]
+// Output: [5,6]
+// Example 2:
+
+// Input: nums = [1,1]
+// Output: [2]
+
+// Constraints:
+
+// n == nums.length
+// 1 <= n <= 105
+// 1 <= nums[i] <= n
+
+// Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<int> ans;
+
+        for (int i = 0; i < n; i++)
+        {
+            int num = abs(nums[i]);
+            int idx = num - 1;
+
+            if (nums[idx] > 0)
+            {
+                nums[idx] *= -1;
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] > 0)
+            {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
+// TC-->O(N)
