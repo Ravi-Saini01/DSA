4529e808064870bf061af52b2b612c8920b716b0
Ravi Saini <rs8228495@gmail.com>
2026-08-31 18:08:14 +0530

    DSA

---
+++ Random_Questions/461.cpp
@@ -0,0 +1,45 @@
+// 461. Hamming Distance
+
+// The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+// Given two integers x and y, return the Hamming distance between them.
+
+// Example 1:
+
+// Input: x = 1, y = 4
+// Output: 2
+// Explanation:
+// 1   (0 0 0 1)
+// 4   (0 1 0 0)
+//        ↑   ↑
+// The above arrows point to positions where the corresponding bits are different.
+// Example 2:
+
+// Input: x = 3, y = 1
+// Output: 1
+
+// Constraints:
+
+// 0 <= x, y <= 231 - 1
+
+// Note: This question is the same as 2220: Minimum Bit Flips to Convert Number.
+
+class Solution
+{
+public:
+    int hammingDistance(int x, int y)
+    {
+        int ans = 0;
+        int nobit = x ^ y;
+
+        for (int i = 0; i < 32; i++)
+        {
+            if (nobit & 1)
+                ans++;
+
+            nobit >>= 1;
+        }
+        return ans;
+    }
+};
+// TC-->O(1) since the loop runs for a fixed number of iterations (32 times for 32-bit integers).
