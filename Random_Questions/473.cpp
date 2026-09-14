69ba5442af3d007ed6f7aa687b44d032fb221dd2
Ravi Saini <rs8228495@gmail.com>
2026-09-14 18:19:03 +0530

    DSA

---
+++ Random_Questions/473.cpp
@@ -0,0 +1,99 @@
+// You are given an integer array matchsticks where matchsticks[i] is the length of the ith matchstick. You want to use all the matchsticks to make one square. You should not break any stick, but you can link them up, and each matchstick must be used exactly one time.
+
+// Return true if you can make this square and false otherwise.
+
+// Example 1:
+
+// Input: matchsticks = [1,1,2,2,2]
+// Output: true
+// Explanation: You can form a square with length 2, one side of the square came two sticks with length 1.
+// Example 2:
+
+// Input: matchsticks = [3,3,3,3,4]
+// Output: false
+// Explanation: You cannot find a way to form a square with all the matchsticks.
+
+// Constraints:
+
+// 1 <= matchsticks.length <= 15
+// 1 <= matchsticks[i] <= 108
+
+class Solution
+{
+public:
+    bool isquare(int i, int s1, int s2, int s3, int s4,
+                 vector<int> &matchsticks)
+    {
+        if (i == matchsticks.size())
+        {
+            if (s1 == 0 && s2 == 0 && s3 == 0 && s4 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        if (matchsticks[i] <= s1)
+        {
+            s1 -= matchsticks[i];
+            if (isquare(i + 1, s1, s2, s3, s4, matchsticks))
+            {
+                return true;
+            }
+            s1 += matchsticks[i];
+        }
+        if (matchsticks[i] <= s2)
+        {
+            s2 -= matchsticks[i];
+            if (isquare(i + 1, s1, s2, s3, s4, matchsticks))
+            {
+                return true;
+            }
+            s2 += matchsticks[i];
+        }
+        if (matchsticks[i] <= s3)
+        {
+            s3 -= matchsticks[i];
+            if (isquare(i + 1, s1, s2, s3, s4, matchsticks))
+            {
+                return true;
+            }
+            s3 += matchsticks[i];
+        }
+        if (matchsticks[i] <= s4)
+        {
+            s4 -= matchsticks[i];
+            if (isquare(i + 1, s1, s2, s3, s4, matchsticks))
+            {
+                return true;
+            }
+            s4 += matchsticks[i];
+        }
+        return false;
+    }
+    bool makesquare(vector<int> &matchsticks)
+    {
+        if (matchsticks.size() < 4)
+            return false;
+        int p = 0;
+        for (auto x : matchsticks)
+        {
+            p += x;
+        }
+        if (p % 4 != 0)
+        {
+            return false;
+        }
+        int side = p / 4;
+        sort(matchsticks.rbegin(), matchsticks.rend());
+        if (isquare(0, side, side, side, side, matchsticks))
+        {
+            return true;
+        }
+        return false;
+    }
+};
+// TC-->O(4^n)
